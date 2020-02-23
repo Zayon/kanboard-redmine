17374fbfaea84cc17c7d382281ec8857afaabb3b
Matthias Petermann <mp@petermann-it.de>
2020-02-23 14:03:35 +0100

    Do some API adjustments to regain compatibility with current Kanboard releases

--- Template/task/import.php
+++ Template/task/import.php
@@ -1,5 +1,5 @@
 <div class="page-header">
-    <h2><?= $this->text->e($project['name']) ?> &gt; <?= t('Redmine Issue') ?><?= $this->task->getNewTaskDropdown($project['id'], $values['swimlane_id'], $values['column_id']) ?></h2>
+    <h2><?= $this->text->e($project['name']) ?> &gt; <?= t('Redmine Issue') ?><?= $this->task->getNewBoardTaskButton(array('id' => $values['swimlane_id']), array('id' => $values['column_id'], 'project_id' => $project['id'] )) ?></h2>
 </div>
 
 <?= $this->form->label(t('Issue ID'), 'number') ?>
